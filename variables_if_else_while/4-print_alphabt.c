85e61a1e7316e7c94556da6134587cbba6def223
Thea33710 <thea33710@gmail.com>
2025-06-16 13:34:20 +0000

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: alphabet without q and e
+ * parameter: void
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'a' ; c <= 'z'; c++)
+{
+if ( c != 'q' && c != 'e')
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
