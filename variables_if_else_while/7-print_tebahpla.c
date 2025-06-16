f763612f75a09d761cbf72d00eccd7a59ed44ccd
Thea33710 <thea33710@gmail.com>
2025-06-16 13:47:11 +0000

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: afficher l'alphabet
+ * parameter: void
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'z'; c <= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
