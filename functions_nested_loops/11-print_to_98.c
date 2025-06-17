1c881088a8e36eea6f077e35b8e6f6991453512f
Thea33710 <thea33710@gmail.com>
2025-06-17 13:33:41 +0000

    task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ *
+ * Description: 'the program's description'
+ * @n: nombre
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+for (; n <= 98;)
+{
+for (; n <= 98; n++)
+{
+if (n != 98)
+{
+printf("%d, ", n);
+}
+if (n == 98)
+{
+printf("%d\n", n);
+}
+}
+}
+for (; n >= 98; n--)
+{
+if (n != 98)
+{
+printf("%d, ", n);
+}
+if (n == 98)
+{
+printf("%d\n", n);
+}
+}
+}
