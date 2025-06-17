3f3211ba571ac14ab1321bb0e3b46acb375a408c
Thea33710 <thea33710@gmail.com>
2025-06-17 13:13:40 +0000

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 k = i * j;
-if (k != 0 && k <= 9)
+if (j != 0 && k <= 9)
 {
 _putchar(' ');
 _putchar(' ');
@@ -31,12 +31,14 @@ _putchar(' ');
 if (k <= 9)
 {
 _putchar(k + '0');
-_putchar(',');
 }
 if (k > 9)
 {
 _putchar(k / 10 + '0');
 _putchar(k % 10 + '0');
+}
+if (j != 9)
+{
 _putchar(',');
 }
 }
