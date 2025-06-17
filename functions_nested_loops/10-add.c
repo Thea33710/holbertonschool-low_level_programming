52789aaf6da379d2ffe9548ff945d90952458b1d
Thea33710 <thea33710@gmail.com>
2025-06-17 13:18:20 +0000

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: add
+ * @i: nombre
+ * @j: nombre
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int i, int j)
+{
+int k;
+k = i + j;
+return (k);
+}
