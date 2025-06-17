4ed4b36636a0f1a584a39bc19d459d2e08278108
Thea33710 <thea33710@gmail.com>
2025-06-17 11:51:49 +0000

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,7 +16,24 @@ int j;
 int k;
 int l;
 
-for (i = 0; i <= 2; i++)
+for (i = 0; i <= 1; i++)
+{
+_putchar(i);
+for (j = 0; j <= 9; j++)
+{
+_putchar(j);
+_putchar(':');
+for (k = 0; k <= 5; k++)
+{
+_putchar(k);
+for (l = 0; l <= 9; l++)
+{
+_putchar(l);
+}
+}
+}
+}
+for (i = 2; i <= 2; i++)
 {
 _putchar(i);
 for (j = 0; j <= 3; j++)
@@ -26,7 +43,7 @@ _putchar(':');
 for (k = 0; k <= 5; k++)
 {
 _putchar(k);
-for (l = 0; l <= 9; j++)
+for (l = 0; l <= 9; l++)
 {
 _putchar(l);
 }
