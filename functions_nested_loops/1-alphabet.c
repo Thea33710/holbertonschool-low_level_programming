8473f10288d4ed06749902b2bbd3392764612941
Thea33710 <thea33710@gmail.com>
2025-06-17 07:51:16 +0000

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: print alphabet
+ * parameter: void
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
