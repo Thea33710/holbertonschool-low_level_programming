9359af5abdbe46ee9ea3654b3a71548c55f7d306
Thea33710 <thea33710@gmail.com>
2025-06-18 13:43:06 +0000

    task 16

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -11,17 +11,41 @@
 int main(void)
 {
 int i;
-unsigned long j = 1;
-unsigned long k = 2;
-unsigned long u = 0;
+unsigned long j_1 = 0;
+unsigned long j_2 = 1;
+unsigned long k_1 = 0;
+unsigned long k_2 = 2;
+unsigned long u_1 = 0;
+unsigned long u_2 = 0;
 
-printf("%lu, %lu", j, k);
-for (i = 2; i < 98; i++)
+printf("%lu, %lu", j_2, k_2);
+for (i = 3; i <= 92; i++)
 {
-u = j + k;
-printf(", %lu", u);
-j = k;
-k = u;
+u_2 = j_2 + k_2;
+printf(", %lu", u_2);
+j_2 = k_2;
+k_2 = u_2;
+}
+
+j_1 = j_2 / 1000000000;
+j_2 = j_2 % 1000000000;
+k_1 = k_2 / 1000000000;
+k_2 = k_2 % 1000000000;
+
+for (i = 93; i <= 98; i++)
+{
+u_1 = j_1 + k_1;
+u_2 = j_2 + k_2;
+if (u_2 >= 1000000000)
+{
+u_1 += 1;
+u_2 -= 1000000000;
+}
+printf(", %lu%09lu", u_1, u_2);
+j_1 = k_1;
+j_2 = k_2;
+k_1 = u_1;
+k_2 = u_2;
 }
 printf("\n");
 return (0);
