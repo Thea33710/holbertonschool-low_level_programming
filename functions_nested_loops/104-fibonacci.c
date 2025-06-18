4c71dbfffa8ac00f55e1c03d97d2eac889b1ec4e
Thea33710 <thea33710@gmail.com>
2025-06-18 13:03:39 +0000

    task 16

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 50 fibonacci
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+unsigned long j = 1;
+unsigned long k = 2;
+
+printf("%lu, %lu", j, k);
+for (i = 2; i < 98; i++)
+{
+j += k;
+k += j;
+printf(", %lu, %lu", j, k);
+}
+printf("\n");
+return (0);
+}
