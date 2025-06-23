d559b44ad6b0908f1ffcf24c6d05ac901db21bb0
Thea33710 <thea33710@gmail.com>
2025-06-23 08:27:22 +0000

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul  - Entry point
+ *
+ * Description: multiplie a et b
+ * @a: un nombre
+ * @b: un nombre
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
