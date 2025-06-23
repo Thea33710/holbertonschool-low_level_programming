99b284fd356cc674ac30727a50c0d072ace271ae
Thea33710 <thea33710@gmail.com>
2025-06-23 10:41:56 +0000

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ *
+ * Description: print a square
+ * @size: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_square(int size)
+{
+int i, j;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
