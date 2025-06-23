a89d892aacc662ccc2734862b202754215732da2
Thea33710 <thea33710@gmail.com>
2025-06-23 10:59:36 +0000

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "math.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+printf("1 ");
+for (i = 2; i <= 100; i++)
+{
+if ((i % 3) == 0)
+{
+printf(" Fizz");
+}
+else if ((i % 5) == 0)
+{
+printf(" Buzz");
+}
+else
+printf(" %d", i);
+}
+printf("\n");
+return (0);
+}
