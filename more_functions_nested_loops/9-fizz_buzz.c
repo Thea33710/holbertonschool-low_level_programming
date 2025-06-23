fcf254046c9c8134efcfe73a242ac1bd20ca42b3
Thea33710 <thea33710@gmail.com>
2025-06-23 11:03:40 +0000

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,11 @@ int i;
 printf("1 ");
 for (i = 2; i <= 100; i++)
 {
-if ((i % 3) == 0)
+if ((i % 3) == 0 && (i % 5) == 0)
+{
+printf(" FizzBuzz");
+}
+else if ((i % 3) == 0)
 {
 printf(" Fizz");
 }
