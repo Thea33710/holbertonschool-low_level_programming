5f3e4109058fce727f22cac1c5a312682f39ed12
Thea33710 <thea33710@gmail.com>
2025-06-24 11:51:20 +0000

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,26 +11,16 @@
 
 int _atoi(char *s)
 {
-int i = 0;
-int j;
 char c, k;
 
 while (*s != '\0')
-{
-s++;
-i++;
-}
-for (j = 0; j < i; j++)
 {
 if (*s == '-' || *s == '+')
 {
 c = *s;
 }
-if (c != 0)
-{
 _putchar(c);
-}
-if (*s >= 0  && *s <= 9)
+if (*s >= '0'  && *s <= '9')
 {
 k = *s;
 _putchar(k + '0');
