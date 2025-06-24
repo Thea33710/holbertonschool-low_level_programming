9ed08e45cc4edb5add9d0f11941ed74638627362
Thea33710 <thea33710@gmail.com>
2025-06-24 11:36:05 +0000

    task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ *
+ * Description: 'the program's description'
+ * @s: char
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _atoi(char *s)
+{
+int i = 0;
+int j;
+char c, k;
+
+while (*s != '\0')
+{
+s++;
+i++;
+}
+for (j = 0; j <= i; j++)
+{
+if (*s == '-' || *s == '+')
+{
+c = *s;
+}
+_putchar(c);
+if (*s >= '0' && *s <= '9')
+{
+k = *s;
+_putchar(k + '0');
+}
+}
+_putchar('\n');
+return (0);
+}
+
