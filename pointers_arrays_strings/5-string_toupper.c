d6e530a0fa967e172722ead56556056b03c3c758
Thea33710 <thea33710@gmail.com>
2025-06-26 07:22:39 +0000

    a finir

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *string_toupper - Entry point
+ *
+ * Description: changes lowercase to uppercase
+ * @s: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *string_toupper(char *s)
+{
+  int i;
+  for (i = 0; s[i] < '\0'; i++)
+    {
+      if (s[i] >=
+    }
+}
