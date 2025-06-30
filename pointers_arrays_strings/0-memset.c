f77dc5f434693a6018db8ca27125b73ee7585a99
Thea33710 <thea33710@gmail.com>
2025-06-30 08:10:40 +0000

    task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memset - Entry point
+ *
+ * Description: memset
+ * @s: string
+ * @b: char
+ * @n: nombre positif
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+  for (i = 0; i < n; i++)
+    {
+      *s = b;
+      s++;
+    }
+  return (s);
+}
