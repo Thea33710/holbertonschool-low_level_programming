b1470d6730dcab7745604d103f4db3345e24154d
Thea33710 <thea33710@gmail.com>
2025-06-30 08:23:35 +0000

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strchr - Entry point
+ *
+ * Description: strchr
+ * @s: string
+ * @c: char
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (0);
+}
