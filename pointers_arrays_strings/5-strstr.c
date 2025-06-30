35408a50a3358c48b70a33849a83f401f3fa9d81
Thea33710 <thea33710@gmail.com>
2025-06-30 10:22:43 +0000

    task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strstr - Entry point
+ *
+ * Description: strstr
+ * @haystack: string
+ * @needle: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+if (*needle == '\0')
+{
+return (haystack);
+}
+while (*haystack != '\0')
+{
+char *s = haystack;
+char *c = needle;
+while (*c != '\0' && *s != '\0' && *s == *c)
+{
+c++;
+s++;
+}
+if (*c == '\0')
+{
+return (haystack);
+}
+haystack++;
+}
+return (NULL);
+}
