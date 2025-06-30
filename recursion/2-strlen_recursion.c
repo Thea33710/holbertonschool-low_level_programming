ff75f343cd32f51ddafcd53c343cb6305807b079
Thea33710 <thea33710@gmail.com>
2025-06-30 13:20:38 +0000

    task 3

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -10,12 +10,9 @@
 
 int _strlen_recursion(char *s)
 {
-  int i = 0;
-  if (*s != '\0')
-    {
-      i++;
-      s++;
-    }
-  _strlen_recursion(s);
-  return(i);
+if (*s == '\0')
+{
+return (0);
+}
+return ((1) + _strlen_recursion(s + 1));
 }
