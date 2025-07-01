f8d5da26142aad592df137d9574caca0eaea1fb4
Thea33710 <thea33710@gmail.com>
2025-07-01 09:20:13 +0000

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * _help - Entry point
+ *
+ * @i: entier
+ * @n: entier
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _help(int i, int n)
+{
+if ((i * i) == n)
+{
+return (i);
+}
+else if ((i * i) > n)
+{
+return (-1);
+}
+else
+return (_help(i + 1, n));
+}
+
+/**
  * _sqrt_recursion - Entry point
  *
  * @n: entier
@@ -10,5 +33,13 @@
 
 int _sqrt_recursion(int n)
 {
-
+if (n == 0 || n == 1)
+{
+return (n);
+}
+if (n < 0)
+{
+return (-1);
+}
+return (_help(1, n));
 }
