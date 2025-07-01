acbcc493bcad8e06cf18581030cba783ecdf22cb
Thea33710 <thea33710@gmail.com>
2025-07-01 10:42:15 +0000

    yask 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: count arguments
+ * @argv: array of pointer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+argc = 1;
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
