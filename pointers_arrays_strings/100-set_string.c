1d9a869caa6783ed1879e2d113d374edd7e8b570
Thea33710 <thea33710@gmail.com>
2025-07-02 09:21:03 +0000

    task 8

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ *
+ * @s: string
+ * @to: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
