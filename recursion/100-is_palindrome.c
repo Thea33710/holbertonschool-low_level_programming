12f3789cbe62d98098e68addd8a9d289c8a0868f
Thea33710 <thea33710@gmail.com>
2025-07-03 14:32:02 +0200

    task 7

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,6 +1,42 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - Entry point
+ *
+ * @s: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+return ((1) + _strlen_recursion(s + 1));
+}
+
+/**
+ * _check_palindrome - Entry point
+ *
+ * @s: string
+ * @start: nombre
+ * @end: nombre
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _check_palindrome(char *s, int start, int end)
+{
+    if (start >= end)
+        return (1);
+    if (s[start] != s[end])
+        return (0);
+    return _check_palindrome(s, start + 1, end - 1);
+}
+
+/**
  * is_palindrome - Entry point
  *
  * @s: string
@@ -10,20 +46,6 @@
 
 int is_palindrome(char *s)
 {
-  if (*s == '\0')
-    {
-      return (1);
-    }
-  if (*s != '\0')
-    {
-      if (is_palindrome(s) == is_palindrome(s + 1))
-	{
-	  is_palindrome(s +1);
-	}
-      if (is_palindrome(s) != is_palindrome(s + 1))
-	{
-	  return (0);
-	}
-    }
-  return (1);
+    int len = _strlen_recursion(s);
+    return _check_palindrome(s, 0, len - 1);
 }
