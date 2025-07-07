34e2cd25f405969ea068ab69ee4d494d49c25c74
Thea33710 <thea33710@gmail.com>
2025-07-07 13:44:04 +0200

    task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_calloc - Entry point
+ *
+ * @nmemb: nombre entier positif
+ * @size: nombre entier positif
+ *
+ * Return: Always 0 (Success)
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	int *arr;
+
+		if (nmemb == 0 || size == 0)
+		{
+			return (NULL);
+		}
+
+	arr = malloc(sizeof(size) * nmemb);
+
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; i < nmemb; i++)
+		{
+			arr[i] = 0;
+		}
+	return (arr);
+}
