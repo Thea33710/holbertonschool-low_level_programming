02cbd2a1846047ba277faec21e126dbd65bb63ef
Thea33710 <thea33710@gmail.com>
2025-07-07 15:58:18 +0200

    task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_dog - Entry point
+ *
+ * @d: struct dog
+ *
+ * Return: Always 0 (Success)
+ */
+
+void free_dog(dog_t *d) 
+{
+		if (d == NULL)
+		{
+			return;
+		}
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
