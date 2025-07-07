bef9a2511e57bb0f25aafd68bae299ebe9bdd578
Thea33710 <thea33710@gmail.com>
2025-07-07 12:36:42 +0200

    task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * *malloc_checked - Entry point
+ *
+ * @b: un nombre positif
+ *
+ * Return: Always 0 (Success)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - Entry point
+ *
+ * @s1: string
+ * @s2: string
+ * @n: un nombre positif
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
