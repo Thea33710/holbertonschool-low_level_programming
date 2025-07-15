a7ee146223e147a7db55232d63f43bae7cb02bf5
Thea33710 <thea33710@gmail.com>
2025-07-15 13:37:55 +0200

    changements

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,6 +13,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, calc;
+	int (*op_func)(int, int);
 	char *operator;
 
 	if (argc != 4)
@@ -20,10 +21,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
-	operator = argv[2];
-
-	if (!operator)
+	if (!operator || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
@@ -31,8 +29,20 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	calc = get_op_func(operator)(num1, num2);
+	op_func = get_op_func(operator);
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
+	if ((operator[0] == '/' || operator[0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	calc = op_func(num1, num2);
 	printf("%d\n", calc);
 	return (0);
 }
