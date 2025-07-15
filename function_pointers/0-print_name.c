44d3a653f8e11c7f8af64337cc4b430d627d144d
Thea33710 <thea33710@gmail.com>
2025-07-15 11:54:20 +0200

    corrections

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Entry point
+ * print_name - Prints a name
  * @name: un nom
  * @f: pointeur vers une fonction
  *
@@ -10,5 +10,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
