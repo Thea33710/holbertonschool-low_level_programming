ef5437402198538909a0770920f7e96d947284c3
Thea33710 <thea33710@gmail.com>
2025-07-28 10:55:28 +0200

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - free the list
+ * @head: the start of node
+ * Return:
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
