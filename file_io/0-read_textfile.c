b852413d15c50f02897dd0f1ac75c9c3a7c916f8
Thea33710 <thea33710@gmail.com>
2025-07-29 14:45:03 +0200

    task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - read a text file and print it
+ * @filename: the name of the file
+ * @letters: the number of charactere read
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r_bytes, w_bytes;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+	r_bytes = read(fd, buffer, letters);
+	if (r_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
+	if (w_bytes == -1 || w_bytes != r_bytes)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	return (w_bytes);
+}
