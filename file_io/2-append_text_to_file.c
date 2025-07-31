41573f008a66298144660dedebb97a01ad3bc9d4
Thea33710 <thea33710@gmail.com>
2025-07-31 14:02:02 +0200

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -23,12 +23,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content[count] == NULL)
+	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
-	while (text_content != '\0')
+	while (text_content[count] != '\0')
 	{
 		count++;
 	}
