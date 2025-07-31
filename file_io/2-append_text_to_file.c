94ce4f2c830bbd0e04e4a9b1ac7a9a6afcde4b2b
Thea33710 <thea33710@gmail.com>
2025-07-31 14:01:12 +0200

    task 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - add text to a file
+ * @filename: the name of the file
+ * @text_content: a string to put in the file
+ * Return: 1 succes or -1 fail
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, y, count = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content[count] == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	while (text_content != '\0')
+	{
+		count++;
+	}
+	y = write(fd, text_content, count);
+	if (y == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
