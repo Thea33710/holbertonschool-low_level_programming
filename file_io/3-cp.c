457784f6226c50fd9d34ce01c0374f4f15613d9f
Thea33710 <thea33710@gmail.com>
2025-08-01 10:26:09 +0200

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -35,13 +35,12 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		erreur_exit(97, "Usage: cp file_from file_to\n", "");
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		erreur_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -59,15 +58,14 @@ int main(int argc, char *argv[])
 	}
 	if (bytes_r == -1)
 	{
-		close(fd_from);
-		close(fd_to);
+		(close(fd_from), close(fd_to));
 		erreur_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
+		(dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100));
+
 	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		(dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100));
+
 	return (0);
 }
