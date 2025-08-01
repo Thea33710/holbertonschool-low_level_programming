90a440e91e73a7086b442565a962521685f96e26
Thea33710 <thea33710@gmail.com>
2025-08-01 10:20:59 +0200

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -42,7 +42,6 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		erreur_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -54,8 +53,7 @@ int main(int argc, char *argv[])
 		bytes_w = write(fd_to, buffer, bytes_r);
 		if (bytes_w != bytes_r)
 		{
-			close(fd_from);
-			close(fd_to);
+			(close(fd_from), close(fd_to));
 			erreur_exit(99, "Error: Can't write to %s\n", argv[2]);
 		}
 	}
@@ -66,8 +64,10 @@ int main(int argc, char *argv[])
 		erreur_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 	if (close(fd_from) == -1)
-		erreur_exit(100, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
 	if (close(fd_to) == -1)
-		erreur_exit(100, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
 	return (0);
 }
